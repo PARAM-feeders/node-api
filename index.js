eef52cff96208a29512e1e7c4d66fd3296566cd1
Rajith <rajith.venu-gopal@mocaclinic.ca>
2021-06-24 19:28:48 -0400

    removed dotenv

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-require('dotenv').config();
 const port = process.env.PORT;
 const env = process.env.SERVER_ENV;
 require('./server/models/db');
