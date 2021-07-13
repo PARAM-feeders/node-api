f9ca4e216d268b62cad75776cd1041b9200931c7
Aditya V <adityaprasadv@gmail.com>
2021-07-13 17:15:41 -0400

    Post API

--- routes/posts.js
+++ routes/posts.js
@@ -1,27 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../middlewares/auth');
-const {check} = require('express-validator');
+const auth = require("../middlewares/auth");
+const { check } = require("express-validator");
 
-const postController = require('../controllers/posts');
+const postController = require("../controllers/posts");
 
 // @route     GET api/contacts
 // @desc      Get all users contacts
 // @access    Private
-router.get('/', auth, postController.getAllPosts);
+router.get("/posts", auth.isAuthenticatedUser, postController.getAllPosts);
 
 // @route     POST api/contacts
 // @desc      Add new contact
 // @access    Private
 router.post(
-  '/',
+  "/posts",
   [
-    auth,
-    [
-      check('name', 'Name is required')
-        .not()
-        .isEmpty(),
-    ],
+    auth.isAuthenticatedUser,
+    [check("name", "Name is required").not().isEmpty()],
   ],
   postController.createPost
 );
@@ -29,16 +25,24 @@ router.post(
 // @route     PUT api/contacts/:id
 // @desc      Update contact
 // @access    Private
-router.put('/:id', auth, postController.updatePost);
+router.put("/posts/:id", auth.isAuthenticatedUser, postController.updatePost);
 
 // @route     DELETE api/contacts/:id
 // @desc      Delete contact
 // @access    Private
-router.delete('/:id', auth, postController.deletePost);
+router.delete(
+  "/posts/:id",
+  auth.isAuthenticatedUser,
+  postController.deletePost
+);
 
 // @route     get api/contacts/:id
 // @desc      Get single contact
 // @access    Private
-router.get('/:id', auth, postController.getSinglePost);
+router.get(
+  "/posts/:id",
+  auth.isAuthenticatedUser,
+  postController.getSinglePost
+);
 
 module.exports = router;
